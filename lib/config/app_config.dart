b033760e16bd44a1e9553917e89be9bdbea03f5b
mathaiaju <angelia.aju.mathai@gmail.com>
2026-02-08 01:17:24 +0530

    config change

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -3,7 +3,6 @@ class AppConfig {
   static const String apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
     defaultValue: 'http://smritim-backend:3000/api',
-    //defaultValue: 'https://2c60-116-68-75-61.ngrok-free.app/api',
     //defaultValue: 'http://localhost:3000/api',
   );
 }
