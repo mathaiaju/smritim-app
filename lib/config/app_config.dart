88a4ba39b09346768be6af813257069c332fd08c
mathaiaju <angelia.aju.mathai@gmail.com>
2026-02-08 01:27:33 +0530

    config change

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -2,7 +2,7 @@ class AppConfig {
   // Default fallback (local dev)
   static const String apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://smritim-backend:3000/api',
+    defaultValue: 'http://34.47.229.228:3000/api',
     //defaultValue: 'http://localhost:3000/api',
   );
 }
